b0bc28683b57c474949fc75aba347e9b3b5af446
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-01-18 10:32:22 -0800

    Chg Sphinx copyright year range to end w/ "present"

--- docs/conf.py
+++ docs/conf.py
@@ -25,21 +25,10 @@ setup_cfg.read(os.path.abspath("../setup.cfg"))
 project = setup_cfg["metadata"]["name"]
 
 author = "The UBC EOAS MOAD Group and The University of British Columbia"
-
-
-import datetime
-
 pkg_creation_year = 2021
-copyright_years = (
-    "{pkg_creation_year}".format(pkg_creation_year=pkg_creation_year)
-    if datetime.date.today().year == pkg_creation_year
-    else "{pkg_creation_year}-{today:%Y}".format(
-        pkg_creation_year=pkg_creation_year, today=datetime.date.today()
-    )
-)
-copyright = "{copyright_years}, {author}".format(
-    copyright_years=copyright_years, author=author
-)
+
+copyright_years = f"{pkg_creation_year} – present"
+copyright = f"{copyright_years}, {author}"
 
 # The full version, including alpha/beta/rc tags
 import moacean_parcels
