bf6a0f4ef13be3e6d4ab3f64669fca3e092de4c6
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-01-18 09:23:27 -0800

    Add SPDX short-form license identifiers
    Communicates license information in portable and machine-readable manner.

--- moacean_parcels/kernels/__init__.py
+++ moacean_parcels/kernels/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# SPDX-License-Identifier: Apache-2.0
+
 from .DeleteParticle import DeleteParticle
